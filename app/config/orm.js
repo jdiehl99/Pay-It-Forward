6997dc66a62d3ad12e4266b85f6f06eee8beea21
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-09 18:15:19 -0400

    Added basic orm for select all, create new, update, and delete.

--- app/config/orm.js
+++ app/config/orm.js
@@ -44,6 +44,60 @@ function objToSql(ob) {
                 }
             cb(result);
         });
+      },
+
+      create: function(table, cols, vals, cb){
+          var queryString = "INSERT INTO " + table;
+
+          queryString += " (";
+          queryString += cols.toString();
+          queryString += ") ";
+          queryString += "VALUES (";
+          queryString += printQuestionMarks(vals.length);
+          queryString += ") ";
+
+          console.log(queryString);
+
+          connection.query(queryString, vals, function(err, result){
+                if(err){
+                    throw err;
+                }
+            cb(result);
+          })
+      },
+
+      update : function(table, objColVals, condition, cb){
+          var queryString = "UPDATE " + table;
+
+          queryString += " SET ";
+          queryString += objToSql(objColVals);
+          queryString += " WHERE ";
+          queryString += condition; 
+
+          console.log(queryString);
+
+          connection.query(queryString, function(err, result){
+                if(err){
+                    throw err;
+                }
+            cb(result);
+          });
+      },
+
+      delete: function(table, id , cb){
+
+        var queryString = "DELETE FROM " + table;
+
+        queryString += " WHERE ";
+        queryString += "id= ";
+        queryString += id;
+
+        connection.query(queryString, function(err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        })
       }
 
 
