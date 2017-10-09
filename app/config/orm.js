e40fafdc86e78266206dfaa35e5ed93349759bd5
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-09 18:02:17 -0400

    Added Select all function to orm.

--- app/config/orm.js
+++ app/config/orm.js
@@ -37,6 +37,16 @@ function objToSql(ob) {
   var orm = {
       all: function(tableName, cb){
           
+        var queryString = "SELECT * FROM " + tableName + ";"
+        connection.query(queryString, function(err, result){
+                if(err){
+                    throw err;
+                }
+            cb(result);
+        });
       }
+
+
+
       }
 
