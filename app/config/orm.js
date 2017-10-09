f5ce7c45cb7f15352a7cb7821af6c6c1d70885f1
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-09 17:57:52 -0400

    Added basic orm template.

--- app/config/orm.js
+++ app/config/orm.js
@@ -1,2 +1,42 @@
+var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+// END printQuestionMarks ____
+
+  // Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+   
+    for (var key in ob) {
+      var value = ob[key];
+      
+      if (Object.hasOwnProperty.call(ob, key)) {
+     
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+  
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    return arr.toString();
+  }
+  // END objToSql ____
+
+  var orm = {
+      all: function(tableName, cb){
+          
+      }
+      }
 
