f58ea2e545b6860d984353b2b530346f1472f524
joeyfboone <joey@urgentmail.com>
2017-10-10 13:34:56 -0400

    updated server.js

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Set ejs.
+var expejs = require("ejs");
 
 // Import routes and give the server access to them.
 var routes = require("./app/controllers/payitforward_controller.js");
