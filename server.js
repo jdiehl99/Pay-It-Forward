709df2605a28434191a3226adea62afc694837eb
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-10 09:15:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/payitforward_controller.js");
+var routes = require("./app/controllers/payitforward_controller.js");
 
 app.use("/", routes);
 
