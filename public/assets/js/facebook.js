a5ba98b5b68e17db7327539e1bba460156fe008b
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-10 11:22:21 -0400

    Update facebook.js

--- public/assets/js/facebook.js
+++ public/assets/js/facebook.js
@@ -66,20 +66,3 @@
     console.log('Name: ' + response.picture);
 
   }
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-}
