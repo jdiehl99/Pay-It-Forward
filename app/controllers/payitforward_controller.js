e118d9d1e7d7a9e99021c7901747b52958a7ebb3
billscarp <billscarp@gmail.com>
2017-10-11 16:30:10 -0400

    new redirects

--- app/controllers/payitforward_controller.js
+++ app/controllers/payitforward_controller.js
@@ -16,10 +16,12 @@ var charity = require("../models/charity_model.js");
 
 // Route to go to home page
 router.get("/", function (req, res) {
-    user.all( function(result){
-    res.render("index", {user: result})
+    user.all(function (result) {
+        res.render("index", {
+            user: result
+        })
     });
-    
+
 });
 
 // Route to user page
@@ -39,12 +41,12 @@ router.get("/facebook", function (req, res) {
 // user login
 router.post("/user/signup", function (req, res) {
     user.create(["first_name", "last_name", "email", "loan_amt", "user_story"], [req.body.first_name, req.body.last_name, req.body.email, req.body.loan_amt, req.body.user_story], function (result) {});
-     
-    user.setDashboardId(req.body.email, function(result2){
-    res.redirect("/user/dashboard/"+result2[0].id);
-    })    
-    
-    
+
+    user.setDashboardId(req.body.email, function (result2) {
+        res.redirect("/user/dashboard/" + result2[0].id);
+    })
+
+
 
 });
 
@@ -52,8 +54,8 @@ router.post("/user/signup", function (req, res) {
 router.post("/donor/signup", function (req, res) {
     donor.create(["first_name", "last_name", "email", "anonymous"], [req.body.first_name, req.body.last_name, req.body.email, req.body.anonymous], function (result) {});
 
-    donor.setDashboardId(req.body.email, function(result2){
-    res.redirect("/donor/dashboard/"+result2[0].id);
+    donor.setDashboardId(req.body.email, function (result2) {
+        res.redirect("/donor/dashboard/" + result2[0].id);
     })
 });
 
@@ -61,17 +63,20 @@ router.post("/donor/signup", function (req, res) {
 // user dashboard
 router.get("/user/dashboard/:id", function (req, res) {
 
-var userId = req.params.id;
+    var userId = req.params.id;
 
-user.allIdInfo(userId, function(userResult){
+    user.allIdInfo(userId, function (userResult) {
 
-charity.all(function(charityResult){
+        charity.all(function (charityResult) {
 
-    res.render("user_dashboard", {user: userResult, charity: charityResult})
-});
+            res.render("user_dashboard", {
+                user: userResult,
+                charity: charityResult
+            })
+        });
 
 
-});
+    });
 
 
 });
@@ -79,19 +84,58 @@ charity.all(function(charityResult){
 // donor dashboard
 router.get("/donor/dashboard/:id", function (req, res) {
 
-var donorId = req.params.id;
+    var donorId = req.params.id;
 
-donor.allIdInfo(donorId, function(donorResult){
+    donor.allIdInfo(donorId, function (donorResult) {
 
-charity.all(function(charityResult){
+        charity.all(function (charityResult) {
 
-    user.all(function(userResults){
-        res.render("donor_dashboard",{donor: donorResult, charity: charityResult, user: userResults})
+            user.all(function (userResults) {
+                res.render("donor_dashboard", {
+                    donor: donorResult,
+                    charity: charityResult,
+                    user: userResults
+                })
+            })
+        })
+
     })
-})
-
-})
 
 });
+
+// Route to login 
+
+router.post("/login", function (req, res) {
+    // console.log(req.body)
+
+    user.setDashboardId(req.body.email, function (userResult) {
+
+        // console.log("Yomomma", userResult[0]) 
+
+        if (userResult[0] == undefined) {
+
+            donor.setDashboardId(req.body.email, function (donorResult) {
+
+                console.log("check donor ", donorResult[0])
+
+                if (donorResult[0] == undefined) {
+
+                    res.redirect("/signup");
+
+                } else {
+
+                    res.redirect("/donor/dashboard/" + donorResult[0].id);
+
+                }
+            })
+
+        } else {
+
+            res.redirect("/user/dashboard/" + userResult[0].id);
+        }
+
+    })
+
+})
 
 module.exports = router;
