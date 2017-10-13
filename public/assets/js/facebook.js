7b06342e1c55a5bd1024ab8ead1c79f988fd2198
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-13 00:15:16 -0400

    ?

--- public/assets/js/facebook.js
+++ public/assets/js/facebook.js
@@ -1,108 +1,108 @@
-// $( document ).ready(function(){
+// // $( document ).ready(function(){
  
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI(response);
-    } else {
-      // The person is not logged into your app or we are unable to tell.
-      $(".status").html('Please log into this app.');
-    }
-  }
+//   function statusChangeCallback(response) {
+//     console.log('statusChangeCallback');
+//     console.log(response);
+//     // The response object is returned with a status field that lets the
+//     // app know the current login status of the person.
+//     // Full docs on the response object can be found in the documentation
+//     // for FB.getLoginStatus().
+//     if (response.status === 'connected') {
+//       // Logged into your app and Facebook.
+//       testAPI(response);
+//     } else {
+//       // The person is not logged into your app or we are unable to tell.
+//       $(".status").html('Please log into this app.');
+//     }
+//   }
 
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+//   function checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//       statusChangeCallback(response);
+//     });
+//   }
 
-  $(".fbCheck").on("click", function(){
-    console.log("fbcheck click");
-    checkLoginState();
-  })
+//   $(".fbCheck").on("click", function(){
+//     console.log("fbcheck click");
+//     checkLoginState();
+//   })
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '289087048261705',
-    cookie     : false,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
-  });
-
-
-  FB.getLoginStatus(function(response) {
-    if(response == "connected"){
-      FB.logout();
-    }else{
-    statusChangeCallback(response);
-    }
-  });
-
-  };
-
-  // Logout Button
-$("#fbLogOut").on("click", function(){
-  fbLogout();
-
-  window.location.href = "/";
-});
-
-  function fbLogout() {
-    FB.getLoginStatus(function(response) {
-        if (response && response.status === 'connected') {
-            FB.logout(function(response) {
-                document.location.reload();
-            });
-        }
-    });
-  }
+//   window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '289087048261705',
+//     cookie     : false,  // enable cookies to allow the server to access 
+//                         // the session
+//     xfbml      : true,  // parse social plugins on this page
+//     version    : 'v2.8' // use graph api version 2.8
+//   });
 
 
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+//   FB.getLoginStatus(function(response) {
+//     if(response == "connected"){
+//       FB.logout();
+//     }else{
+//     statusChangeCallback(response);
+//     }
+//   });
+
+//   };
+
+//   // Logout Button
+// $("#fbLogOut").on("click", function(){
+//   fbLogout();
+
+//   window.location.href = "/";
+// });
+
+//   function fbLogout() {
+//     FB.getLoginStatus(function(response) {
+//         if (response && response.status === 'connected') {
+//             FB.logout(function(response) {
+//                 document.location.reload();
+//             });
+//         }
+//     });
+//   }
 
 
-  function testAPI(response) {
-    console.log('Welcome!  Fetching your information.... ');
-    $("#fbLog").on("click", function(){
-      console.log("fblog clicked");
-    FB.api('/me', {fields: 'name,email,first_name,last_name,picture'}, function(response) {
+//   (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = "//connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
+
+
+//   function testAPI(response) {
+//     console.log('Welcome!  Fetching your information.... ');
+//     $("#fbLog").on("click", function(){
+//       console.log("fblog clicked");
+//     FB.api('/me', {fields: 'name,email,first_name,last_name,picture'}, function(response) {
      
-      console.log(response);
+//       console.log(response);
       
-      $.ajax("/login", {
-        type: "POST",
-        data: { email: response.email}
-    }).then(
-        function (data) {
-          console.log("data coming from backend",data);
-            // Reload the page to get the updated list
-            if (data.status === "donor") {
-              window.location.href = "/donor/dashboard/"+data.id;
-            } else if (data.status === "user") {
-              window.location.href = "/user/dashboard/"+data.id;
-            } else { // send to signup page
-              window.location.href = "/signup";
-            }
-        });
-            // $.post("/login", {email : response.email}, function(data){
-            //   console.log(data);
-            // })
-    });
-  })
-  }
+//       $.ajax("/login", {
+//         type: "POST",
+//         data: { email: response.email}
+//     }).then(
+//         function (data) {
+//           console.log("data coming from backend",data);
+//             // Reload the page to get the updated list
+//             if (data.status === "donor") {
+//               window.location.href = "/donor/dashboard/"+data.id;
+//             } else if (data.status === "user") {
+//               window.location.href = "/user/dashboard/"+data.id;
+//             } else { // send to signup page
+//               window.location.href = "/signup";
+//             }
+//         });
+//             // $.post("/login", {email : response.email}, function(data){
+//             //   console.log(data);
+//             // })
+//     });
+//   })
+//   }
 
 
-// })
+// // })
