d6f67e8ada9b2142de991787fb91a61796b66904
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-12 23:02:55 -0400

    Update facebook.js

--- public/assets/js/facebook.js
+++ public/assets/js/facebook.js
@@ -1,103 +1,108 @@
+$( document ).ready(function(){
 
+
+  $("#fbLog").on("click", function(){
+    checkLoginState();
+  })
+  
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI(response);
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
 
   window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1440200726070603',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.10'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-   FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
-  if (response.status === 'connected') {
-    // Logged into your app and Facebook.
-    testAPI(response);
-  } else {
-    // The person is not logged into your app or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  }
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
+  FB.init({
+    appId      : '289087048261705',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
   });
-}
 
-// Logout Button
-// $("#fbLogOut").on("click", function () {
-//   fbLogout();
 
-//   window.location.href = "/signup";
-// });
-
-function fbLogout() {
-  FB.getLoginStatus(function (response) {
-    if (response && response.status === 'connected') {
-      FB.logout(function (response) {
-        document.location.reload();
-      });
+  FB.getLoginStatus(function(response) {
+    if(response == "connected"){
+      FB.logout();
+    }else{
+    statusChangeCallback(response);
     }
   });
-}
+
+  };
+
+  // Logout Button
+$("#fbLogOut").on("click", function(){
+  fbLogout();
+
+  window.location.href = "/signup";
+});
+
+  function fbLogout() {
+    FB.getLoginStatus(function(response) {
+        if (response && response.status === 'connected') {
+            FB.logout(function(response) {
+                document.location.reload();
+            });
+        }
+    });
+  }
 
 
-function testAPI(response) {
-  console.log('Welcome!  Fetching your information.... ');
-  // $("#fbLog").on("click", function () {
-    FB.api('/me', {
-      fields: 'name,email,first_name,last_name,picture'
-    }, function (response) {
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
 
+  function testAPI(response) {
+    console.log('Welcome!  Fetching your information.... ');
+    $("#fbLog").on("click", function(){
+    FB.api('/me', {fields: 'name,email,first_name,last_name,picture'}, function(response) {
+     
+      console.log(response);
+      
       $.ajax("/login", {
         type: "POST",
-        data: {
-          email: response.email
-        }
-      }).then(
+        data: { email: response.email}
+    }).then(
         function (data) {
-          console.log("data coming from backend", data);
-          // Reload the page to get the updated list
-          if (data.status === "donor") {
-            window.location.href = "/donor/dashboard/" + data.id;
-          } else if (data.status === "user") {
-            window.location.href = "/user/dashboard/" + data.id;
-          } else { // send to signup page
-            window.location.href = "/signup";
-          }
+          console.log("data coming from backend",data);
+            // Reload the page to get the updated list
+            if (data.status === "donor") {
+              window.location.href = "/donor/dashboard/"+data.id;
+            } else if (data.status === "user") {
+              window.location.href = "/user/dashboard/"+data.id;
+            } else { // send to signup page
+              window.location.href = "/signup";
+            }
         });
-      // $.post("/login", {email : response.email}, function(data){
-      //   console.log(data);
-      // })
+            // $.post("/login", {email : response.email}, function(data){
+            //   console.log(data);
+            // })
+    });
+  })
+  }
 
-    })
-  // });
-}
 
+})
